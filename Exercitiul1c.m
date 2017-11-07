ece93510e6b76913cbc0030d6ff259ef9ef7d0f7
Oprisangeorgiana <32987918+Oprisangeorgiana@users.noreply.github.com>
2017-11-07 19:01:36 +0200

    Update Exercitiul1c.m

--- Exercitiul1c.m
+++ Exercitiul1c.m
@@ -2,7 +2,7 @@
 n=25;                     % factor de umplere
 t=0:0.2:2;                % un vector prntru timp care merge de la 0 la 2 secunde, cu pasul de 200 ms
 s= (1-n/100)*square(t*2*pi, n) - n/100; % square se foloseste pentru a reprezenta grafic semnalul dreptunghiular si, deoarece nivelul maxim este diferit de nivelul minim, se va inmulti cu 1-n/100 si se va scadea n/100 pentru a avea 0.5 nivelul maxim si -1 nivelul minim
-figure(2);                % numele figurii
+figure(3);                % numele figurii
 plot (t,s);               %  se vor reprezenta grafic elementele vectorului s în funcţie de elementele vectorului t; 
 xlabel('Timp');           % denumirea axei x
 ylabel('Amplitudine');    % denumirea axei y
