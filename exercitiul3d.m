2fae5e3fb359a0d2d531ce5a3a47f901adb6e228
Oprisangeorgiana <32987918+Oprisangeorgiana@users.noreply.github.com>
2017-11-07 19:51:50 +0200

    Update exercitiul3d.m

--- exercitiul3d.m
+++ exercitiul3d.m
@@ -1,8 +1,8 @@
 figure(10)
 hold on             % reţine graficul curent şi adaugă în aceeaşi fereastră grafică următoarele reprezentări grafice
 title('Semnal dreptunghiular multinivel aleator');
-xlabel('Timp [s]');
-ylabel('A [V]');
+xlabel('Timp');
+ylabel('Amplitudine');
 nivel = [-7 -5 -3 -1 1 3 5 7 ];
 t= 0:0.002:20;      %rezolutia temporara de 2 ms. Daca dorim sa avem rezolutia temporara de 20 ms, respectiv 200 ms vom modifica t-ul astfel: t= 0:0.02:20, respectiv t= 0:0.2:20 
 for n=0:0.25:40
