a739e3682b77c67fd7cd4e9d3c743195e47aba88
Oprisangeorgiana <32987918+Oprisangeorgiana@users.noreply.github.com>
2017-11-07 18:38:19 +0200

    Add files via upload

---
+++ Exercitiul1b.m
@@ -0,0 +1,10 @@
+
+n=25;                       % factor de umplere
+t=0:0.02:2;                 % un vector prntru timp care merge de la 0 la 2 secunde, cu pasul de 2 ms
+s= (1-n/100)*square(t*2*pi, n) - n/100; % square se foloseste pentru a reprezenta grafic semnalul dreptunghiular si, deoarece nivelul maxim este diferit de nivelul minim, se va inmulti cu 1-n/100 si se va scadea n/100 pentru a avea 0.5 nivelul maxim si -1 nivelul minim
+figure(1);                  % numele figurii
+plot (t,s);                 %  se vor reprezenta grafic elementele vectorului s în funcþie de elementele vectorului t; 
+xlabel('Timp');             % denumirea axei x
+ylabel('Amplitudine');      % denumirea axei y
+title('Semnal dreptunghiular periodic cu rezolutia temporara de 20 ms'); %titlul figurii
+grid                        % reteaua de linii
