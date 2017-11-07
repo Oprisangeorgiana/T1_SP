98624bac91d1536b7a0bbc7b0668a75bb8b469b9
Oprisangeorgiana <32987918+Oprisangeorgiana@users.noreply.github.com>
2017-11-07 19:45:35 +0200

    Add files via upload

---
+++ exercitiul3d.m
@@ -0,0 +1,11 @@
+figure(7)
+hold on             % reþine graficul curent ºi adaugã în aceeaºi fereastrã graficã urmãtoarele reprezentãri grafice
+title('Semnal dreptunghiular multinivel aleator');
+xlabel('Timp [s]');
+ylabel('A [V]');
+nivel = [-7 -5 -3 -1 1 3 5 7 ];
+t= 0:0.002:20;      %rezolutia temporara de 2 ms. Daca dorim sa avem rezolutia temporara de 20 ms, respectiv 200 ms vom modifica t-ul astfel: t= 0:0.02:20, respectiv t= 0:0.2:20 
+for n=0:0.25:40
+    y = datasample(nivel, 1);           %cu functia datasample obtinem observatii despre vectorul niv
+    plot(t, y*rectpuls(t-n, 0.25))      %functia rectpuls genereaza un impuls unitate dreptunghiular de durata 1/4
+end
